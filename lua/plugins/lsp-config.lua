7b5cbc4b7bd8ba679a84874f006b498e9d36473d
Mikko Ryynänen <mikko.ryynanen2@gmail.com>
2024-04-20 14:22:18 +0300

    Update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -143,6 +143,9 @@ return {
           -- on_attach =  on_lsp_attach,
           capabilities = capabilities
       })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end
   },
 }

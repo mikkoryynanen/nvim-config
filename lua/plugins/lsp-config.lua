ecc8616187358bcd78f7cec60d7efeaf653d25c4
Mikko Ryynänen <mikko.ryynanen2@gmail.com>
2024-04-20 13:55:55 +0300

    Now working C# LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,35 +1,148 @@
+-- local on_lsp_attach = function(client, bufnr)
+--   local opts = {buffer = bufnr, remap = false}
+--
+--   vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+--   vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+--   vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+--   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--   vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
+--   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+--   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+--   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+--   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+--   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+-- end
+
+
 return {
-  {
-    "williamboman/mason.nvim",
+	{
+    'j-hui/fidget.nvim',
     config = function()
-      require("mason").setup()
+      require('fidget').setup()
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "csharp_ls" },
-      })
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    init = function()
+      -- Disable automatic setup, we are doing it manually
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
     end,
   },
   {
-    "neovim/nvim-lspconfig",
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
+  },
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'hrsh7th/cmp-nvim-lsp-signature-help'},
+      {'saadparwaiz1/cmp_luasnip'},
+    },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- Here is where you configure the autocompletion settings.
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_cmp()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
+      -- And you can configure cmp even more, if you want to.
+      local cmp = require('cmp')
+      local cmp_action = lsp_zero.cmp_action()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        sources = {
+          {name = 'path'},
+          {name = 'nvim_lsp'},
+          {name = 'nvim_lua'},
+          {name = 'luasnip'},
+          {name = 'buffer'},
+          {name = 'nvim_lsp_signature_help'},
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = lsp_zero.cmp_format(),
+        mapping = cmp.mapping.preset.insert({
+          ['<Tab>'] = cmp_action.luasnip_supertab(),
+          ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+          ['<CR>'] = cmp.mapping.confirm({select = true}),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        }),
+        preselect = 'item',
+        completion = {
+          completeopt = 'menu,menuone,noinsert'
+        },
+      })
+      require("luasnip.loaders.from_vscode").load({ paths = { "~/.config/snippets" } })
+    end
+  },
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason-lspconfig.nvim'},
+    },
+    config = function()
+      -- This is where all the LSP shenanigans will live
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      -- lsp_zero.on_attach(function (client, bufnr)
+      --   on_lsp_attach(client, bufnr)
+      -- end)
+      lsp_zero.set_server_config({
+        capabilities = {
+          textDocument = {
+            foldingRange = {
+              dynamicRegistration = false,
+              lineFoldingOnly = true
+            }
+          }
+        }
       })
 
-      lspconfig.csharp_ls.setup({
-        capabilities = capabilities
+      require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+          lsp_zero.default_setup,
+          lua_ls = function()
+            -- (Optional) Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end,
+        }
       })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
+    end
+  },
+  -- Dotnet Roslyn
+	{
+    'jmederosalvarado/roslyn.nvim',
+    dependencies = {'hrsh7th/cmp-nvim-lsp'},
+    config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      require("roslyn").setup({
+          -- on_attach =  on_lsp_attach,
+          capabilities = capabilities
+      })
+    end
   },
 }

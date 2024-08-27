0fa68f3573791f33ebc9db2893dfc406abe2625a
Mikko Ryynanen <mikko.ryynanen2@gmail.com>
2024-08-27 13:24:07 +0300

    Add Typescript support and Updated LSP-Zero keybindings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,18 +1,3 @@
-local lsp_attach = function(client, bufnr)
-  local opts = { buffer = bufnr }
-
-  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-  vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-  vim.keymap.set('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-end
-
 return {
   {
     "j-hui/fidget.nvim",
@@ -102,13 +87,28 @@ return {
     config = function()
       -- This is where all the LSP shenanigans will live
       local lsp_zero = require("lsp-zero")
+
+      local lsp_attach = function(client, bufnr)
+        local opts = { buffer = bufnr }
+
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      end
+
       lsp_zero.extend_lspconfig({
-        lsp_attach = lsp_attach
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
       })
 
-      -- lsp_zero.on_attach(function (client, bufnr)
-      --   on_lsp_attach(client, bufnr)
-      -- end)
       lsp_zero.set_server_config({
         capabilities = {
           textDocument = {
@@ -121,7 +121,12 @@ return {
       })
 
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "omnisharp_mono" },
+        ensure_installed = {
+          "lua_ls",
+          "omnisharp_mono",
+          "basedpyright",
+          "tsserver"
+        },
         handlers = {
           lsp_zero.default_setup,
           lua_ls = function()
@@ -130,6 +135,8 @@ return {
 
             lspconfig.lua_ls.setup(lua_opts)
             lspconfig.omnisharp_mono.setup()
+            lspconfig.basedpyright.setup()
+            lspconfig.tsserver.setup()
           end,
         },
       })
